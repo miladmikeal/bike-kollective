8d90235b90080061262a5add5bd24b7f3fd0943d
Jacob Anderson <anderja8@oregonstate.edu>
2021-01-23 12:21:42 -0700

    added react-native-maps, gMaps API key

--- src/screens/HomeStack/BrowseBikes.jsx
+++ src/screens/HomeStack/BrowseBikes.jsx
@@ -1,18 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Container, Content, Text } from 'native-base';
-
+import MapView from 'react-native-maps';
+import {
+    Button,
+    Col,
+    Container,
+    Grid,
+    Row,
+    Text
+} from 'native-base';
 
 const BrowseBikes = ({ navigation }) => (
     <Container>
-        <Content>
-            <Text>Hello Bike Browsing Screen!</Text>
-            <Button onPress={() =>navigation.push('BikeDetails')}>
-                <Text>To Bike Details</Text>
-            </Button>
-        </Content>
+        <MapView
+            initialRegion={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            }}
+        />
     </Container>
 )
+
+/*
+const BrowseBikes = ({ navigation }) => (
+    <Container>
+        <Grid>
+            <Row>
+                <MapView
+                    initialRegion={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                />
+            </Row>
+            <Row>
+                <Grid>
+                    <Row>
+                        <Text>Hello Bike Browsing Screen!</Text>
+                    </Row>
+                    <Row>
+                        <Button onPress={() =>navigation.push('BikeDetails')}>
+                            <Text>To Bike Details</Text>
+                        </Button>
+                    </Row>
+                </Grid>
+            </Row>
+        </Grid>
+    </Container>
+)
+*/
 
 BrowseBikes.propTypes = {
     navigation: PropTypes.shape({
