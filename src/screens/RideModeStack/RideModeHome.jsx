5ce8a5799e5726a43345a9965e48abd795be977d
Thomas Prange <tjprange@gmail.com>
2021-02-01 12:55:35 -0800

    add map and location to RideModeHome.jsx

--- src/screens/RideModeStack/RideModeHome.jsx
+++ src/screens/RideModeStack/RideModeHome.jsx
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Container, Content, Text } from 'native-base';
+import { Button, Container, Content, Text, Grid, Row, Spinner } from 'native-base';
+import RideModeMap from '../../components/RideModeMap';
+import LocationServices from '../../utility/location';
 
+const RideModeHome = ({ navigation }) => {
+  const [location, setLocation] = useState();
 
-const RideModeHome = ({ navigation }) => (
-  <Container>
-    <Content>
-      <Text>Hello Ride Mode Home Screen!</Text>
-      <Button onPress={() =>navigation.push('DropOffSubmit')}>
-        <Text>To bike drop off submit</Text>
-      </Button>
-    </Content>
-  </Container>
-);
+  useEffect(() => {
+    LocationServices.getCurrentLocation().then((currentLocation) =>
+      setLocation({
+        latitude: currentLocation.coords.latitude,
+        longitude: currentLocation.coords.longitude,
+      })
+    );
+  }, []);
+
+  if (!location) {
+    return (
+      <Container>
+        <Spinner />
+      </Container>
+    );
+  }
+
+  return (
+    <Container>
+      <Grid>
+        <Row>
+          <RideModeMap location={location} />
+        </Row>
+      </Grid>
+      <Content>
+        <Button onPress={() => navigation.push('DropOffSubmit')}>
+          <Text>To bike drop off submit</Text>
+        </Button>
+      </Content>
+    </Container>
+  );
+};
 
 RideModeHome.propTypes = {
   navigation: PropTypes.shape({
-    push: PropTypes.func.isRequired
-  }).isRequired
+    push: PropTypes.func.isRequired,
+    replace: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 export default RideModeHome;
