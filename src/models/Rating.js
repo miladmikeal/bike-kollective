8d57ab6648ffe837fdc8ae77f51c2ea863b62f74
Thomas Prange <tjprange@gmail.com>
2021-02-04 08:45:59 -0800

    add bikeRatings query and comments property to Rating.js

--- src/models/Rating.js
+++ src/models/Rating.js
@@ -1,9 +1,10 @@
 // class definition for Bike entity
 export default class Rating {
-  constructor(id, bikeId, rating) {
+  constructor(id, bikeId, rating, comment) {
     this.id = id; // ID of this document in firestore
     this.bikeId = bikeId; // ID of the bike this rating is for
     this.rating = rating;
+    this.comment = comment;
   }
 
   getRating = () => this.rating;
