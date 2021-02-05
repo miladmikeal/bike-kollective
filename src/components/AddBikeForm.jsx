cef55f70bbfbfa0662ac50332366284751834f15
Jacob Anderson <anderja8@oregonstate.edu>
2021-02-04 21:35:25 -0700

    add combination to first form, skeleton of waiver page.

--- src/components/AddBikeForm.jsx
+++ src/components/AddBikeForm.jsx
@@ -21,85 +21,102 @@ const AddBikeFormSchema = Yup.object().shape({
     (value) => value !== 'Size'
   ),
   keywords: Yup.string(),
+  lock: Yup.number().required('Lock combination is required').test(
+    'is greater than zero',
+    'Lock combination must be greater than zero',
+    (value) => value > 0
+  )
 });
 
-// eslint-disable-next-line no-unused-vars
-const handleSubmit = (values) => {
-  console.log(values);
-};
+const AddBikeForm = ({ navigation }) => {
 
-const AddBikeForm = ({ navigation }) => (
-  <Formik
-    initialValues={initialValues}
-    validationSchema={AddBikeFormSchema}
-    onSubmit={(values) => console.log(values)}
-  >
-    {(formikProps) => (
-      <Content>
-        <Input
-          placeholder='Name'
-          onChangeText={formikProps.handleChange('name')}
-          style={globalStyles.addBikeInput}
-        />
-        {(formikProps.touched.name && formikProps.errors.name) &&
-          <Text style={globalStyles.addBikeErrorText}>
-            {formikProps.errors.name}
-          </Text>
-        }
-        <Input
-          placeholder='Style'
-          onChangeText={formikProps.handleChange('style')}
-          value={formikProps.values.style}
-          style={globalStyles.addBikeInput}
-        />
-        {(formikProps.touched.style && formikProps.errors.style) &&
-          <Text style={globalStyles.addBikeErrorText}>
-            {formikProps.errors.style}
-          </Text>
-        }
-        <View style={globalStyles.addBikePickerView}>
-          <Picker
-            note
-            mode="dropdown"
-            style={globalStyles.addBikePicker}
-            placeholder='Size'
-            selectedValue={formikProps.values.size}
-            onValueChange={formikProps.handleChange('size')}
+  const handleSubmit = (values) => navigation.push('AddBikeWaiver', { values: { values } });
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={AddBikeFormSchema}
+      onSubmit={handleSubmit}
+    >
+      {(formikProps) => (
+        <Content>
+          <Input
+            placeholder='Name'
+            onChangeText={formikProps.handleChange('name')}
+            style={globalStyles.addBikeInput}
+          />
+          {(formikProps.touched.name && formikProps.errors.name) &&
+            <Text style={globalStyles.addBikeErrorText}>
+              {formikProps.errors.name}
+            </Text>
+          }
+          <Input
+            placeholder='Style'
+            onChangeText={formikProps.handleChange('style')}
+            value={formikProps.values.style}
+            style={globalStyles.addBikeInput}
+          />
+          {(formikProps.touched.style && formikProps.errors.style) &&
+            <Text style={globalStyles.addBikeErrorText}>
+              {formikProps.errors.style}
+            </Text>
+          }
+          <View style={globalStyles.addBikePickerView}>
+            <Picker
+              note
+              mode="dropdown"
+              style={globalStyles.addBikePicker}
+              placeholder='Size'
+              selectedValue={formikProps.values.size}
+              onValueChange={formikProps.handleChange('size')}
+            >
+              <Picker.Item label='Size' value='Size' />
+              <Picker.Item label='Child' value='Child' />
+              <Picker.Item label='Small' value='Small' />
+              <Picker.Item label='Medium' value='Medium' />
+              <Picker.Item label='Large' value='Large' />
+              <Picker.Item label='XL' value='XL' />
+            </Picker>
+          </View>
+          {(formikProps.touched.size && formikProps.errors.size) &&
+            <Text style={globalStyles.addBikeErrorText}>
+              {formikProps.errors.size}
+            </Text>
+          }
+          <Input
+            placeholder='keywords (must be comma seperated)'
+            onChangeText={formikProps.handleChange('keywords')}
+            value={formikProps.values.keywords}
+            style={globalStyles.addBikeInput}
+          />
+          {(formikProps.touched.keywords && formikProps.errors.keywords) &&
+            <Text style={globalStyles.addBikeErrorText}>
+              {formikProps.errors.keywords}
+            </Text>
+          }
+          <Input
+            placeholder='Lock Combination'
+            onChangeText={formikProps.handleChange('lock')}
+            value={formikProps.values.lock}
+            style={globalStyles.addBikeInput}
+            keyboardType='numeric'
+          />
+          {(formikProps.touched.lock && formikProps.errors.lock) &&
+            <Text style={globalStyles.addBikeErrorText}>
+              {formikProps.errors.lock}
+            </Text>
+          }
+          <Button
+            onPress={formikProps.handleSubmit}
+            style={globalStyles.addBikeButton}
           >
-            <Picker.Item label='Size' value='Size' />
-            <Picker.Item label='Child' value='Child' />
-            <Picker.Item label='Small' value='Small' />
-            <Picker.Item label='Medium' value='Medium' />
-            <Picker.Item label='Large' value='Large' />
-            <Picker.Item label='XL' value='XL' />
-          </Picker>
-        </View>
-        {(formikProps.touched.size && formikProps.errors.size) &&
-          <Text style={globalStyles.addBikeErrorText}>
-            {formikProps.errors.size}
-          </Text>
-        }
-        <Input
-          placeholder='keywords (must be comma seperated)'
-          onChangeText={formikProps.handleChange('keywords')}
-          value={formikProps.values.keywords}
-          style={globalStyles.addBikeInput}
-        />
-        {(formikProps.touched.keywords && formikProps.errors.keywords) &&
-          <Text style={globalStyles.addBikeErrorText}>
-            {formikProps.errors.keywords}
-          </Text>
-        }
-        <Button
-          onPress={formikProps.handleSubmit}
-          style={globalStyles.addBikeButton}
-        >
-          <Text>Continue</Text>
-        </Button>
-      </Content>
-    )}
-  </Formik>
-);
+            <Text>Continue</Text>
+          </Button>
+        </Content>
+      )}
+    </Formik>
+  )
+};
 
 AddBikeForm.propTypes = {
   navigation: PropTypes.shape({
--- src/screens/AddBikeStack/AddBikeWaiver.jsx
+++ src/screens/AddBikeStack/AddBikeWaiver.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Container, Content, Text } from 'native-base';
+import { Button, Container, Content, Text, Title } from 'native-base';
+import globalStyles from '../../styles/styles';
 
+// TODO axe this comment block
+// console.log(route.params.values.values);
 
-const AddBikeWaiver = ({ navigation }) => (
+const AddBikeWaiver = ({ navigation, route }) => (
   <Container>
     <Content>
+      <Title style={globalStyles.title}>
+        <Text style={globalStyles.titleText}>
+          Release of Interest
+        </Text>
+      </Title>
+      <Text>
+        PLEASE READ CAREFULLY BEFORE SIGNING
+      </Text>
       <Text>Hello Add Bike Waiver!</Text>
       <Button onPress={() =>navigation.push('AddBikeSubmit')}>
         <Text>To add bike submit</Text>
@@ -17,7 +28,19 @@ const AddBikeWaiver = ({ navigation }) => (
 AddBikeWaiver.propTypes = {
   navigation: PropTypes.shape({
     push: PropTypes.func.isRequired
-  }).isRequired
+  }).isRequired,
+  route: PropTypes.shape({
+    params: PropTypes.shape({
+      values: PropTypes.shape({
+        values: PropTypes.shape({
+          name: PropTypes.string.isRequired,
+          style: PropTypes.string.isRequired,
+          size: PropTypes.string.isRequired,
+          keywords: PropTypes.string,
+        }).isRequired
+      }).isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
 export default AddBikeWaiver;
