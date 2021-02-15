268574fb65770faf90a581713421792d9c5282f9
Milad Mikeal <miladmikeal@gmail.com>
2021-02-14 16:54:32 -0800

    setup custom fonts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-else-return */
+/* eslint-disable global-require */
 /* eslint-disable no-console */
-import React from 'react';
+import React, { useState } from 'react';
 import * as firebase from 'firebase';
 import { registerRootComponent } from 'expo';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
 import { LogBox } from 'react-native';
 import Routes from './Routes';
 
@@ -11,17 +15,33 @@ import { AuthProvider } from './context/AuthProvider';
 // Something is setting massive timeouts
 LogBox.ignoreLogs(['Setting a timer']);
 
+const getFonts = () => Font.loadAsync({
+  'roboto-medium': require('../assets/fonts/Roboto-Medium.ttf'),
+  'roboto-bold': require('../assets/fonts/Roboto-Bold.ttf'),
+});
+
 const App = () => {
   if (!firebase.apps.length) {
     console.log('Connected with Firebase');
     firebase.initializeApp(firebaseConfig);
   }
+  const [fontsLoaded, setFontsLoaded] = useState(false);
 
-  return (
-    <AuthProvider>
-      <Routes />
-    </AuthProvider>
-  );
+  if (fontsLoaded) {
+    return (
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    );
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
 };
 
 export default registerRootComponent(App);
