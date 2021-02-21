9058ce0238a85a51bfc7c1b586e984904d149eeb
Jacob Anderson <anderja8@oregonstate.edu>
2021-02-21 11:35:12 -0700

    bike drop off done except for map

--- src/screens/HomeStack/CheckoutConfirmation.jsx
+++ src/screens/HomeStack/CheckoutConfirmation.jsx
@@ -1,21 +1,41 @@
 import React, { useContext } from 'react';
+import { View } from 'react-native';
 import PropTypes from 'prop-types';
 import { Container, Content, Text, Button } from 'native-base';
+import Unorderedlist from 'react-native-unordered-list';
 import { AuthContext } from '../../context/AuthProvider';
 import { checkOutBike } from '../../api/checkBike';
+import Bike from '../../models/Bike';
+import globalStyles from '../../styles/styles';
 
 const CheckoutConfirmation = ({ navigation, route }) => {
   const { currentUser } = useContext(AuthContext);
-  // console.log(currentUser.email);
   const bike = route.params.bike;
   const userEmail = currentUser.email;
   const bikeId = bike.id;
   return (
     <Container>
       <Content>
-        <Text>Hello Checkout Details Screen!</Text>
-        {/* NOTE SHOULD WE CHANGE RIDE MODE STACK NAME YET? */}
+        <View style={styles.dataView} >
+          <Text style={styles.boldText}>Confirmation Details</Text>
+          <Unorderedlist>
+            <Text>Unlock code: {bike.lock}</Text>
+          </Unorderedlist>
+          <Unorderedlist>
+            <Text>Safety: Always perform a full inspection before riding a bike.</Text>
+          </Unorderedlist>
+          <Unorderedlist>
+            <Text>Use: Follow all applicable traffic laws while riding the bike</Text>
+          </Unorderedlist>
+          <Unorderedlist>
+            <Text>Timeline: The bike must be returned within 24 hours</Text>
+          </Unorderedlist>
+          <Unorderedlist>
+            <Text>Return: Lock the bike and follow app instructions when you are ready to return it</Text>
+          </Unorderedlist>
+        </View>
         <Button
+          style={globalStyles.addBikeButton}
           onPress={() => {
             checkOutBike(userEmail, bikeId);
             navigation.navigate('Ride Mode (temporary tab)', {
@@ -31,9 +51,27 @@ const CheckoutConfirmation = ({ navigation, route }) => {
   );
 };
 
+const styles = {
+  boldText: {
+    fontWeight: 'bold'
+  },
+  dataView: {
+    width: '90%',
+    alignSelf: 'center'
+  },
+};
+
 CheckoutConfirmation.propTypes = {
-  navigation: PropTypes.isRequired,
-  route: PropTypes.isRequired,
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired
+  }).isRequired,
+  route: PropTypes.shape({
+    params: PropTypes.shape({
+      bike: PropTypes.shape({
+        bike: PropTypes.instanceOf(Bike).isRequired,
+      }).isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
 export default CheckoutConfirmation;
