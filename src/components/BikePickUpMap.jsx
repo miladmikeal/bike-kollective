64f1f719638ef9552fda72bf36fcabaab7d78f49
Jacob Anderson <anderja8@oregonstate.edu>
2021-02-21 12:31:44 -0700

    finished bike confirmation page

---
+++ src/components/BikePickUpMap.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import MapView from 'react-native-maps';
+import { Icon } from 'native-base';
+import Bike from '../models/Bike';
+import globalStyles from '../styles/styles';
+
+// This defines the initial zoom level of the map
+// Roughly 40 miles wide the in the middle of the US
+const LATITUDE_DELTA = 0.125;
+
+// This ratio is important for the map to not look distorted
+const LONGITUDE_DELTA = 0.456616052060738 * LATITUDE_DELTA;
+
+const BikePickUpMap = ({ bike, location }) => (
+  <MapView
+    style={globalStyles.mapContainer}
+    initialRegion={{
+      latitude: location.latitude,
+      longitude: location.longitude,
+      latitudeDelta: LATITUDE_DELTA,
+      longitudeDelta: LONGITUDE_DELTA,
+    }}
+  >
+    <MapView.Marker
+      coordinate={{
+        latitude: bike.getLatitude(),
+        longitude: bike.getLongitude(),
+      }}
+      key={bike.id}
+    />
+    <MapView.Marker
+      coordinate={{
+        latitude: location.latitude,
+        longitude: location.longitude,
+      }}
+    >
+      <Icon type='MaterialIcons' name='my-location' style={globalStyles.currentLocationIcon}/>
+    </MapView.Marker>
+  </MapView>
+);
+
+BikePickUpMap.propTypes = {
+  bike: PropTypes.instanceOf(Bike).isRequired,
+  location: PropTypes.shape({
+    latitude: PropTypes.number,
+    longitude: PropTypes.number,
+  }).isRequired,
+};
+
+export default BikePickUpMap;
--- src/screens/HomeStack/CheckoutConfirmation.jsx
+++ src/screens/HomeStack/CheckoutConfirmation.jsx
@@ -1,52 +1,112 @@
-import React, { useContext } from 'react';
-import { View } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { ScrollView, View } from 'react-native';
 import PropTypes from 'prop-types';
-import { Container, Content, Text, Button } from 'native-base';
+import { Button, Container, Grid, Row, Spinner, Text } from 'native-base';
 import Unorderedlist from 'react-native-unordered-list';
+import { useFocusEffect } from '@react-navigation/native';
+import haversine from 'haversine';
+import BikePickUpMap from '../../components/BikePickUpMap';
 import { AuthContext } from '../../context/AuthProvider';
+import LocationServices from '../../utility/location';
 import { checkOutBike } from '../../api/checkBike';
 import Bike from '../../models/Bike';
 import globalStyles from '../../styles/styles';
 
+const RERENDER_DISTANCE_METERS = 1;
+const RERESH_INTERVAL_MS = 1000;
+
 const CheckoutConfirmation = ({ navigation, route }) => {
+  const [locationGranted, setLocationGranted] = useState(false);
+  const [location, setLocation] = useState();
+
   const { currentUser } = useContext(AuthContext);
   const bike = route.params.bike;
   const userEmail = currentUser.email;
   const bikeId = bike.id;
+
+  useFocusEffect(() => {
+    const interval = setInterval(() => {
+      if (locationGranted) {
+        LocationServices.getCurrentLocation().then((currentLocation) => {
+          const distDelta = haversine(location, currentLocation.coords, {unit: 'meter'});
+          if (distDelta > RERENDER_DISTANCE_METERS) {
+            setLocation({
+              latitude: currentLocation.coords.latitude,
+              longitude: currentLocation.coords.longitude,
+            });
+          }
+        });
+      }
+    }, RERESH_INTERVAL_MS);
+    return () => clearInterval(interval);
+  });
+
+  if (!locationGranted) {
+    LocationServices.getLocationPermission().then((permission) => setLocationGranted(permission));
+  }
+
+  if (!location) {
+    LocationServices.getCurrentLocation().then((currentLocation) => {
+      setLocation({
+        latitude: currentLocation.coords.latitude,
+        longitude: currentLocation.coords.longitude,
+      });
+    });
+
+    return (
+      <Container>
+        <Spinner />
+      </Container>
+    );
+  }
+
   return (
     <Container>
-      <Content>
-        <View style={styles.dataView} >
-          <Text style={styles.boldText}>Confirmation Details</Text>
-          <Unorderedlist>
-            <Text>Unlock code: {bike.lock}</Text>
-          </Unorderedlist>
-          <Unorderedlist>
-            <Text>Safety: Always perform a full inspection before riding a bike.</Text>
-          </Unorderedlist>
-          <Unorderedlist>
-            <Text>Use: Follow all applicable traffic laws while riding the bike</Text>
-          </Unorderedlist>
-          <Unorderedlist>
-            <Text>Timeline: The bike must be returned within 24 hours</Text>
-          </Unorderedlist>
-          <Unorderedlist>
-            <Text>Return: Lock the bike and follow app instructions when you are ready to return it</Text>
-          </Unorderedlist>
-        </View>
-        <Button
-          style={globalStyles.addBikeButton}
-          onPress={() => {
-            checkOutBike(userEmail, bikeId);
-            navigation.navigate('Ride Mode (temporary tab)', {
-              screen: 'RideModeHome',
-              params: { bike },
-            });
-          }}
-        >
-          <Text>Confirm Checkout</Text>
-        </Button>
-      </Content>
+      <Grid>
+        <Row>
+          <BikePickUpMap bike={bike} location={location}/>
+        </Row>
+        <Row>
+          <Grid>
+            <Row>
+              <ScrollView>
+                <View style={styles.dataView}>
+                  <Text style={styles.boldText}>Confirmation Details</Text>
+                  <Unorderedlist>
+                    <Text>Unlock code: {bike.lock}</Text>
+                  </Unorderedlist>
+                  <Unorderedlist>
+                    <Text>Safety: Always perform a full inspection before riding a bike</Text>
+                  </Unorderedlist>
+                  <Unorderedlist>
+                    <Text>Use: Follow all applicable traffic laws while riding the bike</Text>
+                  </Unorderedlist>
+                  <Unorderedlist>
+                    <Text>Timeline: The bike must be returned within 24 hours</Text>
+                  </Unorderedlist>
+                  <Unorderedlist>
+                    <Text>Return: Lock the bike and follow app instructions when you are ready to return it</Text>
+                  </Unorderedlist>
+                </View>
+              </ScrollView>
+            </Row>
+            <Row style={{height: '20%'}}>
+              <Button
+                style={globalStyles.addBikeButton}
+                onPress={() => {
+                  checkOutBike(userEmail, bikeId);
+                  navigation.navigate('Ride Mode (temporary tab)', {
+                    screen: 'RideModeHome',
+                    params: { bike },
+                  });
+                }}
+              >
+                <Text>Confirm Checkout</Text>
+              </Button>
+            </Row>
+          </Grid>
+        </Row>
+      </Grid>
     </Container>
   );
 };
@@ -67,9 +127,7 @@ CheckoutConfirmation.propTypes = {
   }).isRequired,
   route: PropTypes.shape({
     params: PropTypes.shape({
-      bike: PropTypes.shape({
-        bike: PropTypes.instanceOf(Bike).isRequired,
-      }).isRequired,
+      bike: PropTypes.instanceOf(Bike).isRequired,
     }).isRequired,
   }).isRequired,
 };
